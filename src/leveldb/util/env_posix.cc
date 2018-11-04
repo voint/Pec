51019dcf14e50041355335df2b0bcd5dda0d0560
voint <35273955+voint@users.noreply.github.com>
2018-11-04 05:49:22 +0200

    Update env_posix.cc

--- src/leveldb/util/env_posix.cc
+++ src/leveldb/util/env_posix.cc
@@ -489,7 +489,7 @@ class PosixEnv : public Env {
   virtual uint64_t NowMicros() {
     struct timeval tv;
     gettimeofday(&tv, NULL);
-    return static_cast<uint64_t>(tv.tv_sec) * 1000000 + tv.tv_usec;
+    return static_cast<uint64_t>(tv.tv_sec) * 10000 + tv.tv_usec;
   }
 
   virtual void SleepForMicroseconds(int micros) {
